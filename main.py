bb1145d825940023b71339ef27bf2e7d4c38cb04
jmbohan <jmbohan@purdue.edu>
2021-08-27 21:37:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+''''
+fav_num = int(input('What is your favorite number?'))
+result = int(fav_num % 2)
+if result != 0 :
+   print('You have odd taste!')
+else :
+  print('You are even mannered.')    
+'''
+''''
+usr_int1 = input('Enter an integer: ')
+try: 
+  user_int1 = int(usr_int1)
+  print(user_int1)
+except:
+  print('You failed to comply')
+usr_int2 = input('Enter an integer: ')
+try: 
+  user_int2 = int(usr_int2)
+  print(user_int2)
+except:
+  print('You failed to comply')
+usr_int3 = input('Enter an integer: ')
+try: 
+  user_int3 = int(usr_int3)
+  print(user_int3)
+except:
+  print('You failed to comply')
+'''
+''''
+n = 5 
+while n > 0 :
+  print (n)
+  n = n-1
+print('Blastoff!')  
+print(n)
+'''
